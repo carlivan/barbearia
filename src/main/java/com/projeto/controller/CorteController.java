01438b94dc2c4c6076557644cb54f9ff132e8add
carlivan <ccarlivan@yahoo.com.br>
2025-08-14 14:35:40 -0300

    novo erro cors

--- src/main/java/com/projeto/controller/CorteController.java
+++ src/main/java/com/projeto/controller/CorteController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class CorteController {
     @Autowired
     private CorteService corteService;
